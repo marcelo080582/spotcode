35c3267b9a7d5b9448efae3c9e52505ecb5cc7f4
Marcelo Martins <marcelo.martins@cilia.com.br>
2020-04-19 23:20:08 -0300

    Creating the Favorite Endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
 
+  concern :favoritable do |options|
+    shallow do
+      post "/favorite", { to: "favorites#create", on: :member }.merge(options)
+      delete "/favorite", { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :dashboard, only: :index
@@ -9,7 +16,15 @@ Rails.application.routes.draw do
       resources :search, only: :index
       resources :albums, only: :show do
         resources :recently_heards, only: :create
+        concerns :favoritable, favoritable_type: 'Album'
       end
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'Song'
+      end
+      resources :artists, only: [] do
+        concerns :favoritable, favoritable_type: 'Artist'
+      end
+      resources :favorites, only: :index
     end
   end
 end
