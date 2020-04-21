9e0c49f7474a870ab4c8d15e39c8b6ea58a9850d
Marcelo Martins <marcelo.martins@cilia.com.br>
2020-04-21 12:55:04 -0300

    Including React in the Project

---
+++ app/javascript/packs/spotcode.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <div>Hello Spotcode</div>,
+    document.getElementById('root'),
+  )
+})
