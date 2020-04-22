89ccdb97eb48a53a58be157b1c5855143979d883
Marcelo Martins <marcelo.martins@cilia.com.br>
2020-04-21 23:07:13 -0300

    Including Bulma

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from 'react-bulma-components';
+import 'app.scss';
 
-  const App = () => {
-    return(
-      <Fragment>
-        Hello
-      </Fragment>
-    );
-  }
-  export default App;
+const App = () => {
+  return(
+    <Fragment>
+      <Button color="primary">Testando o Bulma</Button>
+    </Fragment>
+  );
+}
+export default App;
