4ee30de9e7b024965fa61acaa51559159b3d2b3b
Marcelo Martins <marcelo.martins@cilia.com.br>
2020-04-21 23:39:03 -0300

    Including Routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: -> { request.env['PATH_INFO'] == '/' }
 
   protect_from_forgery unless: -> { request.format.json? }
 end
