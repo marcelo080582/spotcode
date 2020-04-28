7ad4aeb755ad747c24a98da0b920bc45d6bdea8e
Marcelo Martins <marcelo.martins@cilia.com.br>
2020-04-27 22:33:37 -0300

    Creating the Superior Logged menu

--- app/javascript/components/common/menu/index.js
+++ app/javascript/components/common/menu/index.js
@@ -1,16 +1,41 @@
 import React, { Fragment } from 'react';
-import { Navbar, Container, Columns, Button } from 'react-bulma-components';
+import { Navbar, Container, Columns, Button, Dropdown } from 'react-bulma-components';
 import logoImage from '../../../assets/images/logo.png';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { useLocation } from 'react-router-dom';
+import { FaUserCircle } from "react-icons/fa";
 
 const ColumnsFullWidth = styled(Columns)`
   width: 100%;
   padding-top: 20px;
   padding-left: 10px;
   padding-bottom: 10px;
+  padding-right: 10px;
 `
 
 const Menu = () => {
+  let actionButton;
+
+  if (useLocation().pathname == '/') {
+    actionButton = <a href='/users/sign_in' className="is-pulled-right is-right">
+                     <Button outlined={true} color="white">ENTRAR</Button>
+                   </a>
+  }
+  else {
+    actionButton = <Dropdown className='is-pulled-right is-right' color='dark' label={<FaUserCircle size="2em" />}>
+                     <Dropdown.Item value="other">
+                       <a href='/users/edit'>
+                         Edit User
+                       </a>
+                     </Dropdown.Item>
+                     <Dropdown.Item value="other">
+                       <a href='/users/sign_out'>
+                         LogOut
+                       </a>
+                     </Dropdown.Item>
+                   </Dropdown>
+  }
+
   return (
     <Fragment>
       <Navbar color="dark">
@@ -22,9 +47,7 @@ const Menu = () => {
             </Columns.Column>
 
             <Columns.Column>
-              <a href='/users/sign_in' className="is-pulled-right is-right">
-                <Button outlined={true} color="white">ENTRAR</Button>
-              </a>
+              {actionButton}
             </Columns.Column>
 
           </ColumnsFullWidth>
