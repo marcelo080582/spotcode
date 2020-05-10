86817fa661adfdd02154b969ff5c06cf78174978
Marcelo Martins <marcelo.martins@cilia.com.br>
2020-05-10 12:31:57 -0300

    Including the navigation bar

--- app/javascript/screens/discovery/index.js
+++ app/javascript/screens/discovery/index.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useState } from 'react';
+import NavbarFooter from '../../components/common/navbar_footer';
 
   const DiscoveryScreen = () => {
     return(
       <Fragment>
-        <div>Discovery Screen</div>
+        <NavbarFooter/>
       </Fragment>
     );
   }
+
   export default DiscoveryScreen;
--- app/javascript/screens/favorites/index.js
+++ app/javascript/screens/favorites/index.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useState } from 'react';
+import NavbarFooter from '../../components/common/navbar_footer';
 
   const FavoritesScreen = () => {
     return(
       <Fragment>
-        <div>Favorites Screen</div>
+        <NavbarFooter/>
       </Fragment>
     );
   }
+
   export default FavoritesScreen;
