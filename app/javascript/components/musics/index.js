42a898e521cfa5c9d98fd1faf6238ede7d21b5a3
Marcelo Martins <marcelo.martins@cilia.com.br>
2020-05-16 18:50:14 -0300

    Music player (Playing the songs)

--- app/javascript/components/musics/index.js
+++ app/javascript/components/musics/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import Music from './music';
 import { Button, Columns } from 'react-bulma-components';
@@ -10,6 +10,29 @@ const PlaySequenceButton = styled(Button)`
 const Musics = (props) => {
   const [songs, setSongs] = useState([]);
   const [playing, setPlaying] = useState([]);
+  const AudioRef = useRef();
+  const [playRandom, setPlayRandom] = useState(false);
+
+  const NextSong = () => {
+    if(playRandom) {
+      let index = Math.floor(Math.random() * props.songs.length);
+      setPlaying(props.songs[index]);
+    } else
+      setPlaying([]);
+  }
+
+  const SwitchRandom = () => {
+    if(playRandom) {
+      setPlayRandom(false);
+      setPlaying([]);
+    } else
+      setPlayRandom(true);
+  }
+
+  useEffect(() => {
+    if(playRandom)
+      NextSong();
+  }, [playRandom]);
 
   useEffect(() => {
     setSongs(props.songs.map((song, key) =>
@@ -22,13 +45,26 @@ const Musics = (props) => {
     ));
   }, [props.songs, playing]);
 
+  useEffect(() => {
+    if (AudioRef.current !== null) {
+      AudioRef.current.pause();
+      AudioRef.current.load();
+      if(playing.id) {
+        AudioRef.current.play();
+      }
+    }
+  }, [playing]);
+
   return (
     <Fragment>
       <Columns className='is-mobile is-centered'>
         <Columns.Column desktop={{size: 2}} mobile={{size: 12}} className='has-text-centered'>
-          <PlaySequenceButton className='is-medium' color='primary' outlined>
-            Tocar em sequência
+          <PlaySequenceButton className='is-medium' color='primary' outlined onClick={() => SwitchRandom()} >
+            {playRandom == true ? 'Parar de tocar' : 'Tocar aleatoriamente'}
           </PlaySequenceButton>
+            <audio controls ref={AudioRef} onEnded={() => NextSong()} className='is-hidden' >
+              <source src={playing.file_url} />
+            </audio>
         </Columns.Column>
       </Columns>
       {songs}
